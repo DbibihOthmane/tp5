54f03045594acc0c43a50e9117fe635ac7868e97
Dbibih Othmane <othmaanedbibih@gmail.com>
2025-10-16 10:59:48 +0100

    Initial commit

---
+++ src/main/java/com/example/presentation/Presentation2.java
@@ -0,0 +1,23 @@
+package com.example.presentation;
+
+import com.example.dao.IDao;
+import com.example.entities.Product;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Presentation2 {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(com.example.util.HibernateConfig.class);
+
+        IDao<Product> productDao = context.getBean(IDao.class);
+
+        Product product = new Product();
+        product.setName("Produit 1");
+        product.setPrice(100.0);
+
+        productDao.create(product);
+
+        System.out.println("Produit sauvegardé : " + product.getName());
+    }
+}
